3e31a43f0d746574610cf512694aa800ba7e23e3
Md. Yasin Kabir <mykabir0@gmail.com>
2017-02-14 16:58:48 -0600

    Latest Update: the relationship is created for the top ters

--- cloudTag/js/column-chart.js
+++ cloudTag/js/column-chart.js
@@ -13,12 +13,14 @@ function columnChart() {
     var height = svgHeight - margin.bottom - margin.top - 100      
 
 
+    var formatDate = d3.time.format("%Y-%m-%d %H:%M");
+
 
       xValue = function(d) { return d[0]; },
       yValue = function(d) { return d[1]; };
       xScale = d3.time.scale()
             .domain(x_domain)
-            .range([width/(noOfHours+noOfHours/2), width]);
+            .range([0, width-(width/(noOfHours+noOfHours/2))]);
             //width/48, (width*46)/49
 
 
@@ -70,7 +72,7 @@ console.log(noOfHours)
 
       // Update the inner dimensions.
       var g = svg.select("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+          .attr("transform", "translate(" + (margin.left+(width/(noOfHours* 1.75)))  + "," + margin.top + ")");
 
           counter = 0;
 var xStart = 0;
@@ -81,27 +83,12 @@ var xStartRed = 0;
       bar.exit().remove();
       bar .attr("class", function(d, i) { return d[1] < 0 ? "bar negative" : "bar positive"; })
           .attr("fill", function(d){ 
-                                     
-                                          //counter!=23
-                                        /*if(1){
-                                          counter++;*/
                                           if(d[1]<0){
                                             
                                          return colorScaleRed(d[1]);
                                             }
                                           else 
-                                            return colorScale(d[1]);
-                                          
-                                         /* else {
-                                         
-                                            counter = -1;
-                                            counter++;
-                                            return "#fff";
-                                          }
-*/
-                                         
-                                          
-
+                                            return colorScale(d[1]);                                   
                                       })
 
           .attr("x", function(d) { /*if(d[1]<0) {
@@ -116,11 +103,27 @@ var xStartRed = 0;
 
           .attr("y", function(d, i) {   return d[1] < 0 ? Y0() : "0"; })
           .attr("width", (svgWidth/noOfHours)-2)
-          .attr("height", function(d, i) { return 225; });
+          .attr("height", function(d, i) { return 225; })
+          .on('click', function(d,i){ 
+                  mousex = d3.mouse(this);
+                  var invertedx = timelineX.invert(mousex[0]);
+                  //var invertedy = timelineY.invert(mousex[1]);
+                  invertedx = invertedx.getTime();
+                  var time = new Date(invertedx).setMinutes(0);
+                  startTimeForRelation = new Date(new Date(time).setSeconds(0));
+
+                  var timeEnd = new Date(invertedx)
+                      timeEndHour = timeEnd.setHours(timeEnd.getHours()+1);
+                      timeEndMinute = new Date(timeEndHour).setMinutes(0);
+                      endTimeForRelation = new Date(new Date(timeEndMinute).setSeconds(0));
+
+                  console.log(startTimeForRelation + " " + endTimeForRelation)
+                  drawRelationSanky();
+              });
 
     // x axis at the bottom of the chart
      g.select(".x.axis.lineXAxis") //".x.axis.lineXAxis"
-        .attr("transform", "translate(0," + height + ")")
+        .attr("transform", "translate("+ 0 +"," + height + ")")
         .call(xAxis)
             .selectAll("text")  
             .attr("x", "-60")
